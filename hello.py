4bd41daa6a78df1e2e61a803767e465228648834
Ryan Scott <ryan.scott@ssaihq.com>
2020-01-06 07:26:38 -0500

    added hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+# python program to learn git from command line, no github
+def hello(msg):
+    # works only on Python 3.7+
+    print(f"Hello {msg}!")
+
+if __name__ == "__main__":
+    hello("world")
